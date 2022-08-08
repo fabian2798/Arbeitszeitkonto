e1fa51a4c5024b07a12ac71ada8a29a5292ed861
Fabian Fregin <fabian.fregin@web.de>
2022-08-08 06:53:22 +0200

    Schwerpunkt: Filecheck

---
+++ Version 1.8.22/mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QRegularExpression>
+#include <QRegularExpressionMatch>
+#include <QFile>
+#include <QFileDialog>
+#include <QIODevice>
+#include <QTextStream>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QString fileName;   
+    void process_line(QString s,QString ftype);
+    QString check_filetyp(QString filename);
+
+private slots:
+    void loadFile();
+
+};
+#endif // MAINWINDOW_H
